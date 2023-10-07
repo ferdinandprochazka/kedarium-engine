c197b2fd583c99320c58b2d62e4a2f20505d4d8d
ferdinandprochazka <ferdinandprochazka@proton.me>
2023-10-07 22:50:33 +0200

    Drawing a colored triangle

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -8,9 +8,25 @@
 #include "Kedarium/Graphics.hpp"
 
 // Constants
+
 const unsigned int WINDOW_WIDTH  = 800;
 const unsigned int WINDOW_HEIGHT = 600;
 const std::string  WINDOW_TITLE  = "Kedarium Engine";
+
+// Vertices and Indices
+const GLfloat vertices[] = {
+  -0.5f,  -0.5f, 0.f, 1.f, 0.f, 0.f,
+   0.0f,  -0.5f, 0.f, 0.f, 1.f, 0.f,
+   0.5f,  -0.5f, 0.f, 0.f, 0.f, 1.f,
+  -0.25f,  0.0f, 0.f, 1.f, 1.f, 0.f,
+   0.25f,  0.0f, 0.f, 1.f, 0.f, 1.f,
+   0.f,    0.5f, 0.f, 0.f, 1.f, 1.f,
+};
+const GLuint indices[] = {
+  0, 1, 3,
+  1, 2, 4,
+  3, 4, 5,
+};
 
 int main()
 {
@@ -68,15 +84,46 @@ int main()
   // Shaders
   kdr::Graphics::Shader defaultShader("resources/Shaders/default.vert", "resources/Shaders/default.frag");
 
+  // VAO, VBO, and EBO
+  GLuint VAO;
+  GLuint VBO;
+  GLuint EBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
   // Main Loop
   while (!glfwWindowShouldClose(window))
   {
     glfwPollEvents();
     glClear(GL_COLOR_BUFFER_BIT);
     defaultShader.Use();
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, NULL);
     glfwSwapBuffers(window);
   }
 
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
   defaultShader.Delete();
   glfwDestroyWindow(window);
   glfwTerminate();
